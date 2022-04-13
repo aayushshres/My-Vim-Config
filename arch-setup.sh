99e3256ed50f97517911e7a553940f1e357e764c
aayushshres <aayushshres20@gmail.com>
2022-04-13 18:28:48 +0545

    Template copying fixed

--- arch-setup.sh
+++ arch-setup.sh
@@ -8,9 +8,9 @@ cp vimrc ~/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir ~/.vim/templates
-cp skeleton.c ~/.vim/templates/skeleton.c
-cp skeleton.cpp ~/.vim/templates/skeleton.cpp
-cp skeleton.sh ~/.vim/templates/skeleton.sh
+cp Templates/skeleton.c ~/.vim/templates/skeleton.c
+cp Templates/skeleton.cpp ~/.vim/templates/skeleton.cpp
+cp Templates/skeleton.sh ~/.vim/templates/skeleton.sh
 vim
 clear
 
