cf7f640b1688ed948ac23e56155eadcc5923e05d
Aayush Shrestha <aayushshres20@gmail.com>
2023-09-19 14:32:17 +0545

    Debian nodejs installation fix

--- setup.sh
+++ setup.sh
@@ -58,13 +58,19 @@ do
         sudo dnf install nodejs
         linux_setup
         input_check=1
-    
+
     elif [[ $osChoice -eq 3 ]] # For Debian
     then
         sudo apt install vim
         sudo apt install git
         sudo apt install cmake
-        sudo apt install nodejs && npm
+        sudo apt install -y ca-certificates curl gnupg
+        sudo mkdir -p /etc/apt/keyrings
+        curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+        NODE_MAJOR=20
+        echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+        sudo apt install nodejs
+        sudo apt install npm
         linux_setup
         input_check=1
 
